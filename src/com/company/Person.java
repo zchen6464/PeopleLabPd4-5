da3e0c27e7468b1120fbc4f742f90dfdee3c3094
Z Chen <zchen6464@bths.edu>
2018-10-05 10:53:05 -0400

    Update Person.java

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,23 +1,35 @@
 package com.company;
 
-public abstract class Person
-{
-    private String firstName;
-    private String familyName;
+public class Person {
+    private String firstName, familyName;
 
-    public Person(String firstname, String familyName)
-    {
-        this.firstName = firstname;
+    public Person(String firstName, String familyName) {
+        this.firstName = firstName;
         this.familyName = familyName;
 
 
     }
-    public abstract String getFirstName();
-    public abstract String getFamilyName();
-    
-    public boolean equals(Person p)
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public void setFamilyName(String familyName)
     {
-        if(firstName.equals(familyName)) {
+        this.familyName = familyName;
+    }
+
+    public boolean equals(Person person)
+    {
+        if(this.firstName.equals(person.getFirstName()) && this.familyName.equals(person.getFamilyName())) {
             return true;
         }
         else
