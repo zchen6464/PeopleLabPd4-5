24d084d2f1f37fe2fd090990fb6eb39b78c19e45
Z Chen <zchen6464@bths.edu>
2018-10-05 10:53:26 -0400

    Add files via upload

---
+++ src/com/company/Student.java
@@ -0,0 +1,34 @@
+package com.company;
+
+public class Student extends Person
+{
+    private double GPA;
+    private String gender, age;
+
+    public Student(String firstName, String familyName,double GPA,String age,String gender)
+    {
+        super(firstName,familyName);
+        this.GPA = GPA;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public double getGPA()
+    {
+        return this.GPA;
+    }
+
+    public String getGender()
+    {
+        return this.gender;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String toString()
+    {
+        return getFamilyName() + getFirstName();
+    }
+}
